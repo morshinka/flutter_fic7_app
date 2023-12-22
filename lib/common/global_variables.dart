43f953ff7414dbd225bc70e4e5cc399c8234ff7c
Morshinka <tegarprarama461@gmail.com>
2023-12-23 04:41:07 +0700

    fix error

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,6 +1,6 @@
 class GlobarVariables {
   // harus dibuah sesuai link yang ada pada ngrok
   static const baseUrl =
-      'https://a933-2001-448a-4063-1ed1-e9d7-7e7d-dde1-f07c.ngrok-free.app';
+      'https://77ad-2001-448a-4063-1ed1-1550-8f25-f66-3dac.ngrok-free.app';
   // end ngrok
 }
